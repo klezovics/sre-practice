e0711a1ba3b7b0c20ba88f0d9c5228cc8f22359d
artursklezovics <klezovich@gmail.com>
2026-01-17 15:31:18 +0100

    Add basic C syntax

--- c/syntax/basics.c
+++ c/syntax/basics.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
+int adjust(int x) {
+    if (x % 2 == 0)
+        return x;
+    else
+        return 1;
+}
+
 int main() {
-    int i;            // variable declaration
+    int i;
     int sum = 0;
 
-    for (i = 1; i <= 5; i++) { // loop
-        if (i % 2 == 0) {      // if
-            sum += i;
-        } else {               // else
-            sum += 1;
-        }
+    for (i = 1; i <= 5; i++) {
+        sum += adjust(i);   // function call
     }
 
     printf("Sum = %d\n", sum);
